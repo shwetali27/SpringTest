b40055a0c7dca9905c51601fca0dadd493291059
shwetali27 <khambeshwetali@ymail.com>
2016-09-19 17:42:09 +0530

    Jdbc test

--- DatabaseDemo/src/main/java/com/bridgelabz/dao/EmployeeShowDataDao.java
+++ DatabaseDemo/src/main/java/com/bridgelabz/dao/EmployeeShowDataDao.java
@@ -6,14 +6,11 @@
 */
 
 package com.bridgelabz.dao;
-import java.awt.List;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.bridgelabz.dto.Employee;
-import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
-
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
@@ -31,10 +28,11 @@ public class EmployeeShowDataDao {
 	}
 	
 	//getting values from database
+	@SuppressWarnings("unchecked")
 	public ArrayList<Employee> getAllEmployees(){
 		return jdbcTemplate.query("select*from employee", new ResultSetExtractor(){
-			public ArrayList extractData(ResultSet rs) throws SQLException, DataAccessException {
-				ArrayList<Employee> list = new ArrayList();
+			public ArrayList<Employee> extractData(ResultSet rs) throws SQLException, DataAccessException {
+				ArrayList<Employee> list = new ArrayList<Employee>();
 				while(rs.next()){
 					Employee employee = new Employee();
 					employee.setId(rs.getInt(1));
